4a6da09b6dd4a1ab9527ce2570667923dcbdd3aa
theapache64 <theapache64@gmail.com>
2024-06-07 15:32:37 +0530

    🌟 bump to  0.0.3

--- src/main/kotlin/com/github/theapache64/dexdiff/ui/splash/SplashViewModel.kt
+++ src/main/kotlin/com/github/theapache64/dexdiff/ui/splash/SplashViewModel.kt
@@ -9,7 +9,7 @@ class SplashViewModel @Inject constructor(
     appRepo: AppRepo
 ) {
     companion object{
-        const val VERSION = "0.0.2"
+        const val VERSION = "0.0.3"
     }
 
     private val _welcomeMsg = MutableLiveData<String>()
